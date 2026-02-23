50a4f237d5a7f9386320dff14e5ff2d4628f1ef7
mohan92 <mohan920228@gmail.com>
2026-02-23 11:24:52 +0000

    adding files

--- main.tf
+++ main.tf
@@ -2,13 +2,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "git_terraform_sg" {
-  name   = "git_terraform_sg"
-  vpc_id = var.vpc_id
-  tags = {
-    name = "git_terraform_sg"
+terraform {
+  backend "s3" {
+    bucket = "703329362-poc-s3 "
+    key    = "sg/terraform.tfvars"
+    region = "us-east-1"
   }
 }
-output "sg_id" {
-  value = aws_security_group.git_terraform_sg.id
+
+resource "aws_security_group_rule" "demo_sg_rule" {
+  security_group_id = aws_security_group.demo_sg
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  type              = "ingress"
+}
+
+resource "aws_security_group" "demo_sg" {
+  name   = "demo_sg_1"
+  vpc_id = var.vpc_id
 }
